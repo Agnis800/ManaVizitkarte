5239b014dc3a5753a25309fe4336176258eba30e
Agnis800 <agnisvanags@gmail.com>
2022-12-15 16:46:33 +0200

    updated Request.php

--- src/Request.php
+++ src/Request.php
@@ -1,9 +1,24 @@
 <?php
 print_r($_GET);
 print_r($_POST);
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == 'POST'){
+    print_r("POST");
+} elseif ($method == 'GET'){
+    print_r("GET");
+} elseif ($method == 'PUT'){
+    print_r("PUT");
+} elseif ($method == 'DELETE'){
+    print_r("DELETE");
+} else {
+    print_r('method unknown');
+}
+
 class Request {
     public $type1 = "get";
     public $type2 = "post";
-    public $_REQUEST_URI;
+    public function uri() {
+      echo $_SERVER['REQUEST_URI'];
+    }
 }
 ?>
